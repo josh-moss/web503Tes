056ef161c4899611c9bed527f3432989a6a3b2da
josh <josh-moss@live.nmit.ac.nz>
2020-09-17 10:52:33 +1200

    added code to index.js file

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,26 @@
 var button = document.getElementById("enter");
+var input = document.getElementById("input");
+var ul = document.querySelector("ul")
+
+function inputLength() {
+    return input.value.length;
+}
+
+function createListElements() {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+
+    li.addEventListener("click", function() {
+        //creates a boolearn that toggles the done class on li:
+        // if the list item is clicked this toggles the done class
+        var finsihed = this.classList.toggle("done");
+        //creates a remove button for the finished item:
+        var removeButton = document.createElement("button");
+        removeButton.classList.add("deleteButton");
+
+        // if the list item is clicked (li add event listener) then
+        // finished is true
+        }
+    }
+}
